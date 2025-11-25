2041bf1ee57a5e5fd1a89f50a45eb6f8c9d05b4b
Dimas26rus <koval26vlg@gmail.com>
2025-11-25 19:48:04 +0300

    Refactor imports to new core engine entrypoints

--- arbitrage_bot/__init__.py
+++ arbitrage_bot/__init__.py
@@ -1,8 +1,9 @@
 """Пакет с компонентами арбитражного бота."""
 
 from arbitrage_bot.core.advanced_arbitrage_engine import AdvancedArbitrageEngine
-from arbitrage_bot.core.advanced_bot import HistoricalReplayer, main
+from arbitrage_bot.core.advanced_bot import HistoricalReplayer
 from arbitrage_bot.core.config import Config
+from arbitrage_bot.core.engine import run_advanced_bot
 from arbitrage_bot.core.optimized_config import OptimizedConfig
 from arbitrage_bot.core.real_trading import RealTradingExecutor
 from arbitrage_bot.exchanges.bybit_client import BybitClient, BybitWebSocketManager
@@ -10,6 +11,9 @@ from arbitrage_bot.exchanges.okx_client import OkxClient
 from arbitrage_bot.monitoring.monitoring import AdvancedMonitor
 from arbitrage_bot.monitoring.visualization import Dashboard
 from arbitrage_bot.strategies.indicator_strategies import StrategyManager
+
+advanced_main = run_advanced_bot
+main = run_advanced_bot
 
 __all__ = [
     "AdvancedArbitrageEngine",
@@ -23,5 +27,7 @@ __all__ = [
     "OptimizedConfig",
     "RealTradingExecutor",
     "StrategyManager",
+    "advanced_main",
     "main",
+    "run_advanced_bot",
 ]
